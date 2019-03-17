43b61bdc13ec249285ee12d46bd913689369eb12
Kaveh Saleminejad <lcaveh@gmail.com>
2019-03-17 10:43:41 -0700

    navigation component and authentication fixed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 import React, { Component } from "react";
 import { commentsRef, authRef, provider } from "./firebase";
-import Navigation from "./components/Navigation"
-import AllComments from "./components/AllComments"
-import CreateComment from "./components/CreateComment"
-import EditComment from "./components/EditComment"
+import "../node_modules/uikit/dist/css/uikit.css";
+import Navigation from "./components/Navigation";
+import AllComments from "./components/AllComments";
+import CreateComment from "./components/CreateComment";
+import EditComment from "./components/EditComment";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: []
+      comments: [],
+      user: null
     };
+    this.signIn = this.signIn.bind(this);
+    this.signOut = this.signOut.bind(this);
   }
-  signIn = () => dispatch => {
-    authRef
-      .signInWithPopup(provider)
-      .then(result => {})
-      .catch(error => {
-        console.log(error);
+  signIn() {
+    authRef.signInWithPopup(provider).then(result => {
+      const user = result.user;
+      this.setState({
+        user
       });
-  };
-  
-  signOut = () => dispatch => {
-    authRef
-      .signOut()
-      .then(() => {
-        console.log("done")
-        // Sign-out successful.
-      })
-      .catch(error => {
-        console.log(error);
+    });
+  }
+  signOut() {
+    authRef.signOut().then(() => {
+      this.setState({
+        user: null
       });
-  };
+    });
+  }
+
+
 
   render() {
     return (
-      <div>
-        <Navigation />
+      <div className="uk-container">
+        <Navigation
+          handleLogin={this.signIn}
+          handleLogout={this.signOut}
+          user={this.state.user}
+        />
         <AllComments />
         <CreateComment />
         <EditComment />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 
 class Navigation extends React.Component {
 render(){
+    console.log(this.props)
     return(
-        <div>Navigation</div>
+        <div className="header">
+        <nav className="uk-navbar uk-navbar-container uk-margin">
+            <div className="uk-navbar-left">
+                <span className="uk-navbar-toggle">{!this.props.user ? (
+                    <span onClick={this.props.handleLogin}>LogIn</span>
+                ) : (
+                        <span onClick={this.props.handleLogout}>
+                            <img
+                                className="uk-comment-avatar uk-border-circle"
+                                src={this.props.user.photoURL}
+                                width="20"
+                                height="20"
+                                alt="Border circle"
+                            />  {this.props.user.displayName.split(' ')[0]}
+                        </span>
+                    )}</span>
+            </div>
+        </nav>
+    </div>
     )
 }
 }
